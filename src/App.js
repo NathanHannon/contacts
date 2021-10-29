cc2d7ecfdf19488068e0f299634f9430a2ead54a
Nathan Hannon <nathan@hannon.me>
2021-10-29 10:59:14 -0400

    added router for future navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+library.add(fab, fas);
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<Router>
+					<div className='NavContainer'>
+
+					</div>
+					<div className="App">
+
+					</div>
+				</Router>
+			</div>
+		);
+	}
 }
 
 export default App;
