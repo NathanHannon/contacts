da3136ee4113b8014ceaa0996f31b49d5a3d882d
Nathan Hannon <nathan@hannon.me>
2021-10-29 16:45:03 -0400

    added Login/Signup Pages with SSO assets

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import { Form, FormGroup, Label, Input } from 'reactstrap';
 
 class Login extends Component {
     render() {
         return (
-            <div>
+            <div className="container">
                 <h1>Login</h1>
+                <hr />
+                <Form>
+                    <FormGroup>
+                        <Label for="email"><h4>Email</h4></Label>
+                        <Input type="email" name="email" id="email" />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="password"><h4>Password</h4></Label>
+                        <Input type="password" name="password" id="password" />
+                    </FormGroup>
+                </Form>
             </div>
         );
     }
